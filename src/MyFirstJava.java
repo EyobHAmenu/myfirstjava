c7774e0cfdda409da0f64faf4d536ab782d4f47a
EyobHAmenu <heyob888@gmail.com>
2020-04-13 14:59:10 -0400

    Second commit

--- src/MyFirstJava.java
+++ src/MyFirstJava.java
@@ -3,7 +3,7 @@
 public class MyFirstJava {
     public static void main (String [] args)
     {
-        System.out.println("Hello World");
-        System.out.println("Work hard to understand Java very well.");
+        System.out.println("Hello Eyob H. Amenu");
+
     }
 }
