d8c7701b520eb009f203cc136b8d1aaf5ec70ad5
EyobHAmenu <heyob888@gmail.com>
2020-04-13 14:07:13 -0400

    First commit

---
+++ src/MyFirstJava.java
@@ -0,0 +1,9 @@
+/* This is my first Java program using IntelliJ IDEA
+* */
+public class MyFirstJava {
+    public static void main (String [] args)
+    {
+        System.out.println("Hello World");
+        System.out.println("Work hard to understand Java very well.");
+    }
+}
